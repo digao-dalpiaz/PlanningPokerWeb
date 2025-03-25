738bfc7fac2e09a608db98beddafc6d099e0e73c
Rodrigo Depiné Dalpiaz <dalpiaze@gmail.com>
2025-03-25 08:52:30 -0300

    Update Program.cs

--- api/Program.cs
+++ api/Program.cs
@@ -2,6 +2,8 @@ using api;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.UseUrls("http://0.0.0.0:8080");
+
 // Add services to the container.
 
 builder.Services.AddControllers();
