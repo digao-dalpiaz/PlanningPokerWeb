9e38e20726281900e3b68a11160e03809177c050
Rodrigo Depiné Dalpiaz <dalpiaze@gmail.com>
2025-03-25 13:22:47 -0300

    Update DadosUser.cs

--- api/Models/DadosUser.cs
+++ api/Models/DadosUser.cs
@@ -1,4 +1,6 @@
-﻿namespace api.Models
+﻿using System.Text.Json.Serialization;
+
+namespace api.Models
 {
     public class DadosUser
     {
@@ -7,6 +9,8 @@
         public Guid Uuid { get; set; }
         public string Token { get; set; }
         public string Nome { get; set; }
+
+        [JsonIgnore]
         public Sala Sala { get; set; }
         public bool Admin { get; set; }
 
