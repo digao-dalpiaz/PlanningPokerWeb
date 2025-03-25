99b44a04ff36efbf64ce12b962b2d76f572570b1
Rodrigo Depiné Dalpiaz <dalpiaze@gmail.com>
2025-03-25 11:49:58 -0300

    Update sala.jsx

--- front/src/sala.jsx
+++ front/src/sala.jsx
@@ -63,7 +63,7 @@ export function Sala() {
     return (
       <Container>
         {cred.idSala && <><i className="fa-solid fa-layer-group" /> Sala: {cred.idSala}
-          &nbsp;<Button size="sm" variant="light" onClick={shareSala}><i className="fa-solid fa-share-nodes" /></Button><br /></>}
+          &nbsp;<Button size="sm" variant="light" title="Compartilhar" onClick={shareSala}><i className="fa-solid fa-share-nodes" /></Button><br /></>}
         <div style={{ height: 10 }} />
         {inner}
       </Container>
