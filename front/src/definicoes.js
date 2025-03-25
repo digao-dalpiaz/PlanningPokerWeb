bf1833f493fff84fb2cadc2625b4ae3a6b5f380e
Rodrigo Depiné Dalpiaz <dalpiaze@gmail.com>
2025-03-24 22:52:48 -0300

    Update definicoes.js

--- front/src/definicoes.js
+++ front/src/definicoes.js
@@ -1,1 +1,5 @@
-export const URL_BACKEND = 'http://localhost:5018';
+export const URL_BACKEND =
+  process.env.NODE_ENV === 'development' ?
+    'http://localhost:5018'
+    :
+    'https://poker.digaodalpiaz.com/api';
